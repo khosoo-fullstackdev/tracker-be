92ea6469357d80648a23a82f0219f7ec02a1abe1
khosoo-fullstackdev <khosoo.fsdev@gmail.com>
2024-03-11 09:21:01 +0800

    asd

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const { Pool } = require("pg");
-const express = require("express");
+// const express = require("express");
 require("dotenv").config();
 
-const app = express();
+// const app = express();
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
 
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
 
-const fs = require("fs");
-const { error } = require("console");
+// const fs = require("fs");
+// const { error } = require("console");
 
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
 const pool = new Pool({
@@ -25,59 +25,59 @@ const pool = new Pool({
   },
 });
 
-app.post("/signup", async (req, res) => {
-  const newUser = req.body;
+// app.post("/signup", async (req, res) => {
+//   const newUser = req.body;
 
-  console.log(newUser);
+//   console.log(newUser);
 
-  const client = await pool.connect();
-  const Query = `INSERT INTO users (name, email, password, id) VALUES ('${newUser.name}','${newUser.email}','${newUser.password}','${newUser.id}');`;
-  try {
-    client.query(Query);
-  } catch (e) {
-    console.log(e);
-  } finally {
-    client.release();
-    console.log("user add successfully");
-  }
-
-  res.status(200).send({ message: "User Added successfully" });
-});
-
-app.post("/login", async (req, res) => {
-  const user = req.body;
-  console.log(user);
-  const client = await pool.connect();
-  const Query = `SELECT * FROM users WHERE (email='${user.email}' AND password='${user.password}');`;
-
-  try {
-    const dbResponse = await client.query(Query);
-    if (dbResponse["rowCount"]) {
-      return res.status(200).send({ success: "true" });
-    } else {
-      return res.status(500).send({ success: "false" });
-    }
-  } catch (e) {
-    console.log(e);
-  } finally {
-    client.release();
-    console.log();
-  }
-});
-// async function getPgVersion() {
 //   const client = await pool.connect();
+//   const Query = `INSERT INTO users (name, email, password, id) VALUES ('${newUser.name}','${newUser.email}','${newUser.password}','${newUser.id}');`;
 //   try {
-//     const result = await client.query(
-//       "ALTER TABLE users ALTER COLUMN id TYPE VARCHAR(50);"
-//     );
-//   } catch (error) {
-//     console.log(error);
+//     client.query(Query);
+//   } catch (e) {
+//     console.log(e);
 //   } finally {
 //     client.release();
+//     console.log("user add successfully");
 //   }
-// }
-// getPgVersion();
 
-app.listen(4000, () => {
-  console.log("Server is listening on port 4000");
-});
+//   res.status(200).send({ message: "User Added successfully" });
+// });
+
+// app.post("/login", async (req, res) => {
+//   const user = req.body;
+//   console.log(user);
+//   const client = await pool.connect();
+//   const Query = `SELECT * FROM users WHERE (email='${user.email}' AND password='${user.password}');`;
+
+//   try {
+//     const dbResponse = await client.query(Query);
+//     if (dbResponse["rowCount"]) {
+//       return res.status(200).send({ success: "true" });
+//     } else {
+//       return res.status(500).send({ success: "false" });
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   } finally {
+//     client.release();
+//     console.log();
+//   }
+// });
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(
+      "ALTER TABLE category RENAME COLUMN nmae to name;"
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.release();
+  }
+}
+getPgVersion();
+
+// app.listen(4000, () => {
+//   console.log("Server is listening on port 4000");
+// });
