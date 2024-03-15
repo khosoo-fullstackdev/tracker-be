52093c9ba80c5d21745f0789d19cabc6face0a82
khosoo-fullstackdev <khosoo.fsdev@gmail.com>
2024-03-15 12:34:52 +0800

    asd

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,18 @@
 const categoryRouter = require("express").Router();
 
-const {} = require("../service/category-service.js");
+const { getCategory, addCategory } = require("../service/category-service.js");
+
+categoryRouter.post("/getCategory", async (req, res) => {
+  const result = await getCategory();
+
+  res.json(result);
+});
+
+categoryRouter.post("/addCategory", async (req, res) => {
+  const category = req.body;
+  const result = await addCategory(category);
+  res.json(result);
+});
 
 module.exports = {
   categoryRouter,
--- service/category-service.js
+++ service/category-service.js
@@ -14,22 +14,31 @@ const pool = new Pool({
   keepAlive: true,
 });
 
-async function cateAdd() {
+async function getCategory() {
   let response;
   const client = await pool.connect();
-  const Query =
-    "CREATE TABLE users ( id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), email VARCHAR(50) UNIQUE NOT NULL, name VARCHAR(50) NOT NULL , password TEXT, avatar_img TEXT, createdAt TIMESTAMP, updatedAt TIMESTAMP, currencyType TEXT DEFAULT'MNT', balance VARCHAR(255) DEFAULT 0);";
+  const Query = "SELECT * FROM category";
   try {
     response = await client.query(Query);
   } catch (error) {
     console.log(error);
   } finally {
     client.release();
-    return { message: "table created" };
+    return response.rows;
   }
-  // "CREATE TABLE transaction (id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), user_id UUID, FOREIGN KEY(user_id) REFERENCES users(id), description TEXT, amount REAL NOT NULL, transaction ENUM(“INC”, ”EXP”), createdAt TIMESTAMP DEFAULT: NOW() , updatedAt TIMESTAMP DEFAULT: NOW(), category_id UUID FOREIGN KEY(category_id) REFERENCES category(id)";
-  // "CREATE TABLE users ( id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), email VARCHAR(50) UNIQUE NOT NULL, name VARCHAR(50) NOT NULL , password TEXT, avatar_img TEXT, createdAt TIMESTAMP, updatedAt TIMESTAMP, currencyType TEXT DEFAULT'MNT', balance VARCHAR(255) DEFAULT 0);";
-  // "CREATE TABLE category ( id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), name VARCHAR(100) NOT NULL , description TEXT, createdAt TIMESTAMP, updatedAt TIMESTAMP, category_image TEXT";
+}
+async function addCategory(category) {
+  let response;
+  const client = await pool.connect();
+  const Query = `INSERT INTO category (name, description, category_image) VALUES ('${category.name}', '${category.description}', '${category.icon}');`;
+  try {
+    response = await client.query(Query);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.release();
+    return { message: "category added" };
+  }
 }
 
-module.exports = { cateAdd };
+module.exports = { getCategory, addCategory };
